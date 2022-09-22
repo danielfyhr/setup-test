8541a243043d6e5d7bf743176c753a96d14d298d
danielfyhr <danielfyhr@gmail.com>
2022-09-22 22:33:35 +0200

    feat: install homebrew

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 echo "yes, this is dog"
 pwd
+echo "is Command Line Tools installed?"
+xcode-select -p
+echo "installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "Homebrew installed"
+brew doctor
+xcode-select -p
 echo "Work complete"
